47e6ef07fdae055d426cc37f9838d6340b5cee64
esmanurgokkaya <esmanurgkky10@gmail.com>
2024-08-01 15:44:04 +0300

    .

--- Api/BookLibary.Api/obj/Debug/net8.0/BookLibary.Api.AssemblyInfo.cs
+++ Api/BookLibary.Api/obj/Debug/net8.0/BookLibary.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibary.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9978359933af65869b6bee9f77efb84991d71aa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff11b3168f7d20e205a8a98a74736bf4a8bc92f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibary.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibary.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
