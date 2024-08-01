90615822a504979d0bb56572819b47155735049d
eyupcan0 <eyupcanekizz@gmail.com>
2024-08-01 14:53:40 +0300

    updatecontroller eklendi

---
+++ Api/BookLibary.Api/Services/AuthServices/UpdateServices/IUpdateService.cs
@@ -0,0 +1,11 @@
+﻿using BookLibary.Api.Models;
+using BookLibary.Api.Repositories;
+
+namespace BookLibary.Api.Services.AuthServices.UpdateServices
+{
+	public interface IUpdateService
+	{
+		Task<GetOneResult<User>> GetUserByIdAsync(string id);
+		Task<GetOneResult<User>> UpdateUserAsync(string id, User user);
+	}
+}
---
+++ Api/BookLibary.Api/Services/AuthServices/UpdateServices/UpdateService.cs
@@ -0,0 +1,27 @@
+﻿using BookLibary.Api.Models;
+using BookLibary.Api.Repositories;
+
+namespace BookLibary.Api.Services.AuthServices.UpdateServices
+{
+	public class UpdateService : IUpdateService
+	{
+		private readonly IRepository<User> _updateRepository;
+		public GetManyResult<User> GetAllUsers()
+		{
+			return _updateRepository.GetAll();
+		}
+		public UpdateService(IRepository<User> updateRepository)
+		{
+			_updateRepository = updateRepository;
+		}
+		public Task<GetOneResult<User>> GetUserByIdAsync(string id)
+		{
+			return _updateRepository.GetByIdAsync(id);
+		}
+		public async Task<GetOneResult<User>> UpdateUserAsync(string id, User user)
+		{
+			return await _updateRepository.ReplaceOneAsync(user, id);
+		}
+		
+	}
+}
